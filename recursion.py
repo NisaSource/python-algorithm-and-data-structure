a1a54fab1de879b4ef8f847d67242e9712d0869e
Nisa Khaerunnisa <nisa@Nisas-MacBook-Pro.local>
2021-01-05 16:15:21 +0800

    Added count consonant algorithm to the recursion

--- recursion.py
+++ recursion.py
@@ -2,15 +2,31 @@
 
 str1 = "iLovepython" # return L
 str2 = "ilovePython" # return P
-str3 = "ilovepython" # return "No uppercase string"
+str3 = "ilove programming" # return "No uppercase string"
 
 def firstUpperCase(inputStr, idx = 0):
     if inputStr[idx].isupper():
         return inputStr[idx]
     if idx == len(inputStr) - 1:
-        return "No uppercase string"
+        return "No uppercase"
     return firstUpperCase(inputStr, idx + 1)
 
 print(firstUpperCase(str1))
 print(firstUpperCase(str2))
 print(firstUpperCase(str3))
+
+# Count consonant in the string
+
+vowels = ["a","i","u","e","o"]
+
+def consonantSum(inputStr):
+    if inputStr == '':
+        return 0
+    if inputStr[0].lower() not in vowels and inputStr[0].isalpha():
+        return 1 + consonantSum(inputStr[1:])
+    else:
+        return consonantSum(inputStr[1:])
+
+print(consonantSum(str1))
+print(consonantSum(str2))
+print(consonantSum(str3))
