4c5bb0783f616a2a8b871e4679d184b599956bac
Nisa Khaerunnisa <nisa@Nisas-MacBook-Pro.local>
2021-01-13 18:38:01 +0800

    Added decimalToBinary

--- recursion.py
+++ recursion.py
@@ -88,3 +88,16 @@ def greatCommonDivisor(a, b):
         return greatCommonDivisor(b, a % b)
 
 print(greatCommonDivisor(326, 278))
+
+
+# 7. Decimal to Binary
+
+# N = 10 return 1010
+
+def decimalToBinary(n):
+    assert int(n) == n, "ONLY POSITIVE INTEGER!"
+    if n == 0:
+        return 0
+    return (n % 2) + (10 * decimalToBinary(int(n / 2)))
+
+print(decimalToBinary(10))
