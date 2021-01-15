b3f7badaf1caca777cbf3f6047423f8bcb3df543
Nisa Khaerunnisa <nisa@Nisas-MacBook-Pro.local>
2021-01-15 22:21:48 +0800

    Added nestedEvenSum

--- recursion.py
+++ recursion.py
@@ -186,3 +186,25 @@ def capitalizeFirst(a):
     return result + capitalizeFirst(a[1:])
 
 print(capitalizeFirst(A))
+
+
+# 12. Write a recursive function called nestedEvenSum. Return the sum of all
+# even numbers in an object which may contain nested objects
+
+obj1 = {
+  "a": 2,
+  "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+  "c": {"c": {"c": 2}, "cc": 'ball', "ccc": 5},
+  "d": 1,
+  "e": {"e": {"e": 2}, "ee": 'car'}
+} # return 10
+
+def nestedEvenSum(obj, sum=0):
+    for k in obj:
+        if type(obj[k]) is dict:
+            sum += nestedEvenSum(obj[k])
+        if type(obj[k]) is int and obj[k] % 2 == 0:
+            sum += obj[k]
+    return sum
+
+print(nestedEvenSum(obj1))
