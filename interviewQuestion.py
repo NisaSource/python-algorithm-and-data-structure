56a149b71c661462f368102b7e7f9f2ededdc9aa
Nisa Khaerunnisa <nisa@Nisas-MacBook-Pro.local>
2021-01-25 18:45:00 +0800

    Added findMaxProduct

--- interviewQuestion.py
+++ interviewQuestion.py
@@ -1,4 +1,4 @@
-# 1. How to find missing number from list of number from 1 to 100 ?
+# 1. Find missing number from list of number from 1 to 100
 
 
 myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
@@ -37,4 +37,24 @@ def findPairs(list, n):
 res = findPairs(myList2, 6)
 for a in res:
     print(a)
+
+
+# 3. Find maximum product of two integers in the array where all elements are positive
+
+import numpy as np
+
+myArray = np.array([2,30,12,45,34,54,13,27,59,32,21,39,40,5,9]) #return 
+
+def findMaxProduct(arr):
+    maxProduct = 0
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] * arr[j] > maxProduct:
+                maxProduct = arr[i] * arr[j]
+                pairs = str(arr[i]) + "," + str(arr[j])
+    print(pairs)
+    return maxProduct
+
+print(findMaxProduct(myArray))
+
 
