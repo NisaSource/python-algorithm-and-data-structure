6f16f55073892c00335ef6e96d4e5856583407f3
Nisa Khaerunnisa <nisa@Nisas-MacBook-Pro.local>
2021-01-26 16:31:00 +0800

    Added findBestScore

--- arrayList.py
+++ arrayList.py
@@ -3,7 +3,18 @@
 
 myList = [1,2,3,4,5,6] #return [2,3,4,5]
 
-def middle(list):
+def findMiddleList(list):
     return list[list[0]:list[-2]]
 
-print(middle(myList))
+print(findMiddleList(myList))
+
+# 2. Given a list, write a function to get first and second best scores from the list.
+
+myList2 = [67,86,45,87,59,97,98,52,41] # return 98 97
+
+def findBestScores(list):
+    list.sort()
+    
+    return list[-1], list[-2]
+
+print(findBestScores(myList2))
