0d0b8333ec18f7748ba6a825664b9a0fa11d47a8
NisaSource <khaerunnisa2201@gmail.com>
2021-02-17 17:34:29 +0800

    Added LinkedList class

---
+++ dataStructure/LinkedList.py
@@ -0,0 +1,56 @@
+from random import randint
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return str(self.value)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+    def __str__(self):
+        values = [str(x.value) for x in self]
+        return ' -> '.join(values)
+
+    def __len__(self):
+        result = 0
+        node = self.head
+        while node:
+            result += 1
+            node = node.next
+        return result
+
+    def add(self, value):
+        if self.head is None:
+            newNode = Node(value)
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+        return self.tail
+
+    def generate(self, n, minVal, maxVal):
+        self.head = None
+        self.tail = None
+        for i in range(n):
+            self.add(randint(minVal, maxVal))
+        return self
+
+linkedList = LinkedList()
+linkedList.generate(10,0,50)
+
+print(linkedList)
+print(len(linkedList))
